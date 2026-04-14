90cbf06e21461c083f8922d65ad4ba00c602a91a
bsujithaapthitech <sujith.b2602@gmail.com>
2026-04-14 13:40:53 +0530

    update

--- app/src/main/java/com/example/pdfreaderapp/MainActivity.kt
+++ app/src/main/java/com/example/pdfreaderapp/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     // PDF viewer instance
     private lateinit var pdfView: PDFView
 
+
+
+
     // Stores last opened page (basic state handling)
     private var lastPage = 0
 
